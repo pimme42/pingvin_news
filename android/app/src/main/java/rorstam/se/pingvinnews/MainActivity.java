0ca1552258ed39e1569b83f15c53ce8d1e99e056
pimme <tobias@rorstam.se>
2019-01-28 17:47:38 +0100

    Initial commit

---
+++ android/app/src/main/java/rorstam/se/pingvinnews/MainActivity.java
@@ -0,0 +1,13 @@
+package rorstam.se.pingvinnews;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
